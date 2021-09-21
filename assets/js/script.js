808fab5c6176903ff4ed7366c93afe30fe646a91
Kurdiamina3 <kurdiamina3@gmail.com>
2021-09-21 17:03:35 -0400

    cleaning up grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,50 +44,6 @@ var loadTasks = function() {
 };
 
 var saveTasks = function() {
-  $(".list-group").on("click", "p", function(){
-    var text = $(this)
-    .text()
-    .trim();
-
-    var textInput = $("<textarea>")
-      .addClass("form-control")
-      .val(text);
-
-    $(this).replaceWith(textInput); 
-    textInput.trigger("focus");
-
-    $(".list-group").on("blur", "textarea", function() {
-      // get the text area's current value/text
-      var text = $(this)
-      .val()
-      .trim();
-
-      // get the parent ul's id attribute
-      var status = $(this)
-      .closest(".list-group")
-      .attr("id")
-      .replace("list-", "");
-
-      // get the task's position in the list of other li elements
-      var index = $(this)
-      .closest(".list-group-item")
-      .index();
-
-      tasks[status][index].text = text;
-      saveTasks();
-      //recreae p element
-      var taskP = $("<p>")
-      .addClass("m-1")
-      .text(text);
-      //replace textarea with p element
-      $(this).replaceWith(taskP);
-    })
-
-    });
-
-
-
-
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
@@ -129,18 +85,47 @@ $("#task-form-modal .btn-primary").click(function() {
 
 });
 
-// remove all tasks
-$("#remove-tasks").on("click", function() {
-  for (var key in tasks) {
-    tasks[key].length = 0;
-    $("#list-" + key).empty();
-  }
 
-  saveTasks();
+$(".list-group").on("click", "p", function(){
+  var text = $(this)
+  .text()
+  .trim();
+
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+
+  $(this).replaceWith(textInput); 
+  textInput.trigger("focus");
+
 });
 
-// load tasks for the first time
-loadTasks();
+  $(".list-group").on("blur", "textarea", function() {
+    // get the text area's current value/text
+    var text = $(this)
+    .val()
+    .trim();
+
+    // get the parent ul's id attribute
+    var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+    // get the task's position in the list of other li elements
+    var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+    tasks[status][index].text = text;
+    saveTasks();
+    //recreae p element
+    var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+    //replace textarea with p element
+    $(this).replaceWith(taskP);
+  });
 
 // due date was clicked
 $(".list-group").on("click", "span", function(){
@@ -191,4 +176,21 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
-})
+});
+
+
+
+// remove all tasks
+$("#remove-tasks").on("click", function() {
+  for (var key in tasks) {
+    tasks[key].length = 0;
+    $("#list-" + key).empty();
+  }
+
+  saveTasks();
+});
+
+// load tasks for the first time
+loadTasks();
+
+
